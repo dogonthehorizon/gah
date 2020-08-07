635218661fff370a1af3c8b2ce53fa73a1ce71f6
Fernando Freire <dogonthehorizon@gmail.com>
2020-08-07 06:05:46 -0700

    Refactor to use conduit for requests

--- src/Network/Github/Api.hs
+++ src/Network/Github/Api.hs
@@ -4,11 +4,16 @@ module Network.Github.Api where
 import           Control.Monad.Reader   (MonadReader, ask)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Text (Text)
-import Network.HTTP.Req (runReq, NoReqBody(..), defaultHttpConfig, req, GET(..), responseBody, bsResponse, (/:), https, header, Option, oAuth2Token)
+import Network.HTTP.Req (runReq, NoReqBody(..), defaultHttpConfig, req, GET(..), responseBody, bsResponse, (/:), https, header, Option, oAuth2Token, reqBr)
 import qualified Data.Text.Encoding as T
 import qualified Data.ByteString.Char8 as B
 import Gah.Monad
 import Control.Lens ((^.))
+import Network.HTTP.Req.Conduit
+import qualified Data.Conduit.Binary as CB
+import Data.Conduit ((.|), runConduitRes)
+import Conduit (filterC)
+import Codec.Archive.Zip.Conduit.UnZip (unZipStream)
 
 -- TODO support honoring rate limits
 
@@ -22,13 +27,17 @@ userAgent = header "User-Agent" "gah/0.0.1"
 accept :: Option https
 accept = header "Accept" "application/vnd.github.v3+json"
 
+noEntries (Left _) = false
+noEntries (Right _) = true
+
 -- TODO embed req in Gah monad via MonadHttp
 logs :: (MonadReader ctx m, HasApiToken ctx Text, MonadIO m) => m ()
 logs = do
   ctx <- ask
   let token = (ctx ^. apiToken)
   runReq defaultHttpConfig $ do
-    bs <- req GET (https github /: "repos" /: "armory-io" /: "dinghy" /: "actions" /: "runs" /: "186342587" /: "logs") NoReqBody bsResponse (userAgent <> accept <> (oAuth2Token (T.encodeUtf8 token)))
-    -- TODO this currently returns a zip archived file, need to use mkkarpov/zip to fiddle with
-    --      potentially via https://hackage.haskell.org/package/req-conduit-1.0.0/docs/Network-HTTP-Req-Conduit.html
-    liftIO . B.putStrLn $ responseBody bs
+    let url = https github /: "repos" /: "armory-io" /: "dinghy" /: "actions" /: "runs" /: "186342587" /: "logs"
+    let headers = userAgent <> accept <> (oAuth2Token (T.encodeUtf8 token))
+    reqBr GET url NoReqBody headers $ \r ->
+      runConduitRes $
+        responseBodySource r .| unZipStream .| filterC noEntries .| CB.sinkFile "my-file.bin"
